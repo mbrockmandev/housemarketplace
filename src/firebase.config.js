d202d0de9a3a734094c92058743e0e7cae470953
Michael Brockman <mbrockmandev@gmail.com>
2023-03-17 19:02:42 -0400

    add register user

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: 'house-marketplace-app-3b0e9.firebaseapp.com',
   projectId: 'house-marketplace-app-3b0e9',
   storageBucket: 'house-marketplace-app-3b0e9.appspot.com',
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // initialize firebase
+initializeApp(firebaseConfig);
 export const db = getFirestore();
