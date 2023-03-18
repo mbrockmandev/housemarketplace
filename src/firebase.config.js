1e98c8f042223d1dcac612ec9437858a11a4d880
Michael Brockman <mbrockmandev@gmail.com>
2023-03-18 09:16:59 -0400

    update createlisting form

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_appId,
 };
 
+// console.log('firebaseConfig:', firebaseConfig);
+
 // initialize firebase
 initializeApp(firebaseConfig);
 export const db = getFirestore();
